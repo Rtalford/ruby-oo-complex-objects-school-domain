ee10013a94556729946369e39f0278c85a9b75d0
Reynelle <Rtalford13@gmail.com>
2020-12-28 14:20:27 -0500

    commit tree 455b5e4e1769b6aacccad2ea2e1d498011e18b75

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+require 'pry'
+class School 
+   attr_reader :school, :roster, :student_name
+   attr_writer :school, :roster, :student_name
+
+   def initialize(school)
+    @school = school 
+    @roster = {}
+   end 
+
+   def add_student(student_name, grade)
+      #expected: {10=>["AC Slater"]}
+      roster[grade] ||= []
+      roster[grade] << student_name
+   end 
+
+   def grade (grade)
+      roster.detect do |a, b| 
+         if a == grade
+            return b
+         end 
+      end 
+   end 
+
+   def sort 
+      new_hash = {}
+      roster.each do |a, b|
+      new_hash[a] = b.sort
+      end 
+   new_hash 
+
+   end 
+
+end 
